96575fc738b725111be2306f2e6e69f6ead860ac
clc babischkin <n/a>
2020-10-27 14:12:56 -0500

    Initial Commit

---
+++ MiniApp_BurgerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniApp_BurgerApp
+//
+//  Created by Tiger Coder on 10/27/20.
+//  Copyright © 2020 clc babischkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
